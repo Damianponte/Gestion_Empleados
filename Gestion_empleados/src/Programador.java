1c371b720e88323277b024d9c710498680324bdc
dami <damianponte@hotmail.es>
2026-05-06 20:36:33 +0200

    creada atributos y metodos de la clase programador

--- Gestion_empleados/src/Programador.java
+++ Gestion_empleados/src/Programador.java
@@ -1,11 +1,21 @@
 public class Programador extends Empleado implements Evaluable{
 
-    public Programador(String nombre, String apellido, double sueldo) {
+    private int lineasCodigo;
+    private String lenguajePrincipal;
+
+    public Programador(String nombre, String apellido, double sueldo, int lineasCodigo, String lenguajePrincipal) {
         super(nombre, apellido, sueldo);
+        this.lineasCodigo = lineasCodigo;
+        this.lenguajePrincipal = lenguajePrincipal;
     }
 
     @Override
     public double evaluarRendimiento() {
-        return 0;
+        return lineasCodigo / 100.0;
+    }
+
+    @Override
+    public void mostrarInfo() {
+        super.mostrarInfo();
     }
 }
