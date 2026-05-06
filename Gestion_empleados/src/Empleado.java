05bc961c5b96d987471654887b4ade86b8468318
dami <damianponte@hotmail.es>
2026-05-06 20:17:00 +0200

    añadido atributos y metodos

--- Gestion_empleados/src/Empleado.java
+++ Gestion_empleados/src/Empleado.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
-
 public class Empleado {
+
+    private String nombre;
+    private String apellido;
+    private double sueldo;
+    private final int id;
+    private static int contador;
+
+    public Empleado(String nombre, String apellido, double sueldo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.sueldo = sueldo;
+        this.id = contador;
+        contador++;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void mostrarInfo(){
+        System.out.println(nombre + apellido + "con el identificador de : "
+                + id + " tiene un sueldo de: " + sueldo);
+
+    }
 }
