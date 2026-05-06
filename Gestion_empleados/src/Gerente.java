e2e44a3ccbb40c1645b975bd2d71277b0804b85d
dami <damianponte@hotmail.es>
2026-05-06 20:43:44 +0200

    creada atributos y metodos de la clase programador

--- Gestion_empleados/src/Gerente.java
+++ Gestion_empleados/src/Gerente.java
@@ -1,4 +1,29 @@
-public class Gerente {
+public class Gerente  extends Empleado implements Evaluable{
 
+private int numeroProyectos;
+private int numeroEmpleadosACargo;
 
+    public Gerente(String nombre, String apellido, double sueldo, int numeroProyectos, int numeroEmpleadosACargo) {
+        super(nombre, apellido, sueldo);
+        this.numeroProyectos = numeroProyectos;
+        this.numeroEmpleadosACargo = numeroEmpleadosACargo;
+    }
+
+    @Override
+    public double evaluarRendimiento() {
+        return 0;
+    }
+
+    public int getNumeroProyectos() {
+        return numeroProyectos;
+    }
+
+    public int getNumeroEmpleadosACargo() {
+        return numeroEmpleadosACargo;
+    }
+
+    @Override
+    public void mostrarInfo() {
+        super.mostrarInfo();
+    }
 }
